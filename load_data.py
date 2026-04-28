8a7da10a4ad92da860c901872f004551796740e3
Chris Stevenson <chris.stevenson@live.com>
2026-04-27 22:42:14 -0600

    Fill project rows

--- load_data.py
+++ load_data.py
@@ -1,20 +1,33 @@
-from db import engine
-from sqlalchemy import inspect
-from models import Base
+from sqlalchemy.orm import Session
+from csv import DictReader
+from pathlib import Path
+from db import engine, SessionLocal
+from models import Base, Project
+
+CSV_PATH = Path("cell-count.csv")
 
 
 def initialize_db():
     Base.metadata.create_all(bind=engine)
-    inspector = inspect(engine)
-    print(inspector.get_table_names())
-    for table_name in inspector.get_table_names():
-        print(table_name)
-        for column in inspector.get_columns(table_name):
-            print(" ", column["name"], column["type"], "nullable=", column["nullable"])
 
 
-def load_data(): ...
+def _fill_projects(session: Session):
+    with CSV_PATH.open("r", newline="") as f:
+        reader = DictReader(f)
+        project_names = set()
+        for row in reader:
+            project_names.add(row["project"])
+    session.add_all(Project(name=name) for name in project_names)
+    session.commit()
+
+
+def load_data():
+    with SessionLocal() as session:
+        _fill_projects(session)
+        # _fill_subjects()
+        # _fill_samples()
 
 
 if __name__ == "__main__":
     initialize_db()
+    load_data()
