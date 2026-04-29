93b8b664cc3b3528011d6b76d763844db41268c5
Chris Stevenson <chris.stevenson@live.com>
2026-04-28 19:20:51 -0600

    Require pandas; fix nix c deps links using documented workaround

--- flake.nix
+++ flake.nix
@@ -11,21 +11,33 @@
   }: let
     forAllSystems = nixpkgs.lib.genAttrs [
       "x86_64-linux"
-      "aarch_64-linux"
+      "aarch64-linux"
       "x86_64-darwin"
-      "aarch_64-darwin"
+      "aarch64-darwin"
     ];
   in {
     devShells = forAllSystems (system: let
       pkgs = import nixpkgs {inherit system;};
+
+      linuxRuntimeLibs = with pkgs; [
+        stdenv.cc.cc.lib # libstdc++.so.6
+        zlib
+        glib
+      ];
     in {
       default = pkgs.mkShell {
-        packages = with pkgs; [
-          python312
-          uv
-          ruff
-          python312Packages.ty
-        ];
+        packages = with pkgs;
+          [
+            python312
+            uv
+            ruff
+            ty
+          ]
+          ++ nixpkgs.lib.optionals pkgs.stdenv.isLinux linuxRuntimeLibs;
+
+        shellHook = nixpkgs.lib.optionalString pkgs.stdenv.isLinux ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath linuxRuntimeLibs}:$LD_LIBRARY_PATH
+        '';
       };
     });
   };
